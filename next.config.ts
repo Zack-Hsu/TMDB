6146ac8cade3c003dc0d4187c8066d682d90ba9a
zack_hsu <zack_hsu@draytek.com>
2025-02-26 18:12:54 +0800

    更新部屬檔，next export 文件根目錄

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
   output: "export",
+  basePath: '/TMDB', // 設定 GitHub Pages 的子目錄
+  assetPrefix: '/TMDB/', // 確保靜態資源的路徑正確
   images: {
     unoptimized: true,
     domains: ["image.tmdb.org"], // 允許 TMDB 的圖片來源
