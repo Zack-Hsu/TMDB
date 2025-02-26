d081578357adf7867cb2c86779939c16b164e21c
zack_hsu <zack_hsu@draytek.com>
2025-02-26 15:28:01 +0800

    next.config update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
+  output: "export",
   images: {
+    unoptimized: true,
     domains: ["image.tmdb.org"], // 允許 TMDB 的圖片來源
   }
 };
