31e0d147bb281579e9972c0816b2349b7f2ec7fa
Zack-Hsu <zackxu841127@gmail.com>
2025-03-04 22:03:56 +0800

    新增spinner 元件

--- src/elements/components/Spinner/Spinner.tsx
+++ src/elements/components/Spinner/Spinner.tsx
@@ -1,17 +1,11 @@
-import { RootState } from "@/store"
-import { useSelector } from "react-redux"
-
-export default function Spinner() {
-    const { fetchMovieLoader } = useSelector((state: RootState) => state.searchMovie)
-    if (fetchMovieLoader) {
+export default function Spinner(props: { loaderReduxState?: boolean }) {
+    if (props.loaderReduxState) {
         return (
-            <>
-                <div style={{ position: 'fixed', bottom: '10%', left: '50%', zIndex: "10011", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <div className="spinner-border" role="status">
-                    </div>
-                    <span className="sr-only w-100">Loading...</span>
+            <div className="loadding">
+                <div className="spinner-border" role="status">
                 </div>
-            </>
+                <span className="sr-only w-100 text-center">Loading...</span>
+            </div>
         )
     } else {
         return null
